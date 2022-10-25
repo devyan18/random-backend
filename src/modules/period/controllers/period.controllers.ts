309ae60d820df21ebc138f8db81b69d008e9b1db
devyan18 <epache17@gmail.com>
2022-10-25 10:42:24 -0300

    add issues from period module

--- src/modules/period/controllers/period.controllers.ts
+++ src/modules/period/controllers/period.controllers.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express'
+import {
+  createPeriod,
+  deletePeriod,
+  getAllPeriod,
+  getPeriodById,
+  updatePeriod
+} from '../services/period.services'
 
 export async function listPeriodsCtrl (_req: Request, res: Response) {
   try {
-    res.send('success')
+    const periods = await getAllPeriod()
+    res.status(200).json({
+      periods
+    })
   } catch (e) {
     res.status(400).send({ error: 'getAll Periods' })
   }
@@ -10,7 +20,11 @@ export async function listPeriodsCtrl (_req: Request, res: Response) {
 
 export async function getPeriodCtrl (req: Request, res: Response) {
   try {
-    res.send('success')
+    const period = await getPeriodById(req.params.id)
+    if (!period) return res.sendStatus(404)
+    res.status(200).json({
+      period
+    })
   } catch (e) {
     res.status(400).send({ error: 'get PeriodById' })
   }
@@ -18,7 +32,11 @@ export async function getPeriodCtrl (req: Request, res: Response) {
 
 export async function createPeriodCtrl (req: Request, res: Response) {
   try {
-    res.send('success')
+    const carrerId = req.params.career
+    const period = await createPeriod(carrerId, req.body)
+    res.status(200).json({
+      period
+    })
   } catch (e) {
     res.status(400).send({ error: 'create Period' })
   }
@@ -26,7 +44,11 @@ export async function createPeriodCtrl (req: Request, res: Response) {
 
 export async function updatePeriodCtrl (req: Request, res: Response) {
   try {
-    res.send('success')
+    const periodId = req.params.period
+    const updatedPeriod = await updatePeriod(periodId, req.body)
+    res.status(200).json({
+      updatedPeriod
+    })
   } catch (e) {
     res.status(400).send({ error: 'update Period' })
   }
@@ -34,7 +56,11 @@ export async function updatePeriodCtrl (req: Request, res: Response) {
 
 export async function deletePeriodCtrl (req: Request, res: Response) {
   try {
-    res.send('success')
+    const periodId = req.params.period
+    const deletedPeriod = await deletePeriod(periodId)
+    res.status(200).json({
+      deletedPeriod
+    })
   } catch (e) {
     res.status(400).send({ error: 'delete Period' })
   }
--- src/modules/period/services/period.services.ts
+++ src/modules/period/services/period.services.ts
@@ -1,39 +1,43 @@
-export async function getAllPeriod () {
-  try {
-    // ...
-  } catch (error) {
+import CareerModel from '../../career/models/career.model'
+import PeriodModel from '../models/period.model'
+import Period from '../interfaces/period.entity'
 
-  }
+export async function getAllPeriod () {
+  return PeriodModel.find()
 }
 
 export async function getPeriodById (id: string) {
-  try {
-    // ...
-  } catch (error) {
-
-  }
+  return PeriodModel.findById(id)
 }
 
-export async function createPeriod (Period: {}) {
-  try {
-    // ...
-  } catch (error) {
+export async function createPeriod (
+  careerId: string,
+  {
+    periodName,
+    startDate,
+    endDate
+  }: Period) {
+  const period = await PeriodModel.create({
+    periodName,
+    startDate,
+    endDate
+  })
 
-  }
+  const career = await CareerModel.findById(careerId)
+
+  if (!career) throw new Error('Career not found')
+
+  career.periods.push(period._id)
+
+  await career.save()
+
+  return period
 }
 
-export async function updatePeriod (Period: {}) {
-  try {
-    // ...
-  } catch (error) {
-
-  }
+export async function updatePeriod (periodId: string, period: Period) {
+  return PeriodModel.findByIdAndUpdate(periodId, period)
 }
 
 export async function deletePeriod (id: {}) {
-  try {
-    // ...
-  } catch (error) {
-
-  }
+  return PeriodModel.findByIdAndDelete(id, { new: true })
 }
